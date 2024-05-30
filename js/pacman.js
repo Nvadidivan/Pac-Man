2b4939da0dd8f352d9e38daa074f8edb03e71030
Nvadidivan <navid18lang@gmail.com>
2024-05-30 11:43:56 -0400

    FIX: Pacman movement

--- js/pacman.js
+++ js/pacman.js
@@ -5,13 +5,14 @@ class PacMan {
     ry;
     constructor() {
         this.x = 15
-        this.y = 23
+        this.y = 24
         this.rx = this.x
         this.ry = this.y
         this.dx = 0
         this.dy = 0
     }
     move(e){
+        console.log(this.x, this.y, "DDDD")
         if (e.key == "w" && grid.nodes[this.y - 1][this.x].walkable) {
             this.dy = -1;
             this.dx = 0;
@@ -33,35 +34,51 @@ class PacMan {
 
     update() {
 
-        this.rx += this.dx * timePassed
-        this.ry += this.dy * timePassed
-
+        this.rx += this.dx * timePassed * 5
+        this.ry += this.dy * timePassed * 5
+        console.log(this.x, this.y)
 
         if (this.dx == 0) {
-            this.rx = Math.round(this.rx)
+            this.rx = this.x
         }
         if (this.dy == 0) {
-            this.ry = Math.round(this.ry)
+            this.ry = this.y
         }
 
-        if (this.x > 1) {
+        if (this.dx > 1) {
             this.x = 29
-        } else if (this.x < -1) {
+        } else if (this.dx < -1) {
             this.x = 0
-        } else if (this.x > 0) {
+        } else if (this.dx > 0) {
             this.x = Math.floor(this.rx)
-        } else if (this.x < 0) {
+            if (grid.nodes[this.y][this.x + 1].isWall) {
+                this.dx = 0
+            }
+        } else if (this.dx < 0) {
             this.x = Math.ceil(this.rx)
-        } else if (this.y > 0) {
+            if (grid.nodes[this.y][this.x - 1].isWall) {
+                this.dx = 0
+            }
+        } else if (this.dy > 0) {
             this.y = Math.floor(this.ry)
-        } else if (this.y < 0) {
+            if (grid.nodes[this.y + 1][this.x].isWallg) {
+                this.dy = 0
+            }
+        } else if (this.dy < 0) {
             this.y = Math.ceil(this.ry)
+            if (grid.nodes[this.y - 1][this.x].isWall) {
+                this.dy = 0
+            }
+        }
+
+        if (!grid.nodes[this.y][this.x].walkable) {
+            console.log("wtf")
         }
     }
 
     draw(){
         ctx.beginPath();
-        ctx.rect(16 * (this.rx - 1), 16 * this.ry, 16, 16);
+        ctx.rect(16 * (this.rx - 1), 16 * (this.ry - 1), 16, 16);
         ctx.fillStyle = "#FFFF00";
         ctx.fill();
         ctx.closePath();
